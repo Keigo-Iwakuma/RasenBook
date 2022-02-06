dadd7c51c536c1d83be54b80eda9007ec0e66f9b
Keigo-Iwakuma <sym90chn@gmail.com>
2022-02-06 23:43:16 +0900

    add 4 files.

---
+++ Courses/ITP1/03B-PrintTestCases.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int index = 1;
+    while (true) {
+        int x;
+        cin >> x;
+        if (x == 0) break;
+        cout << "Case " << index << ": " << x << endl;
+        index++;
+    }
+}
