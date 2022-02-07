f152b8ab0832207a16f53529cbd4e58fac02720a
Keigo-Iwakuma <sym90chn@gmail.com>
2022-02-07 22:21:37 +0900

    add 4 files.

---
+++ Courses/ITP1/05D-StructuredProgramming.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void call(int n) {
+    int i = 0;
+    while (++i <= n) {
+        int x = i;
+        if (x % 3 == 0) {
+            cout << ' ' << i;
+        } else {
+            while (x) {
+                if (x % 10 == 3) {
+                    cout << ' ' << i;
+                    break;
+                }
+                x /= 10;
+            }
+        }
+    }
+
+    cout << endl;
+}
+
+int main() {
+}
